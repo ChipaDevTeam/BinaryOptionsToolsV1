090c15865309a909f7d918ba26fdb192f4b516fd
Rick-29 <91347677+Rick-29@users.noreply.github.com>
2025-01-12 00:27:56 +0100

    Update getdatatest.py

--- getdatatest.py
+++ getdatatest.py
@@ -1,7 +1,7 @@
 from BinaryOptionsTools import pocketoption
 
 ssid = (r'42["auth",{"session":"n6ghkt8nk931jj6ffljoj8knj3","isDemo":1,"uid":85249466,"platform":2}]')
-api = PocketOption(ssid, True)
+api = pocketoption(ssid, True)
 
 df = api.GetCandles("AUDNZD_otc", 1, count=9000, count_request=100)
 df.to_csv("history-AUDNZD_otc.csv")
